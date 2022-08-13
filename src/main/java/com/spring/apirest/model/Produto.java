51e5aeb7a2ebfc1f4ea8b7b8f0d2f436a572cd08
ulissesgomes82 <ulissesdeveloper82@gmail.com>
2022-08-12 21:57:16 -0300

    adicionado a dependencia drivepostgre, configurado o acesso ao BD e configurado a classe produto

--- src/main/java/com/spring/apirest/model/Produto.java
+++ src/main/java/com/spring/apirest/model/Produto.java
@@ -3,9 +3,19 @@ package com.spring.apirest.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private Double preco;
