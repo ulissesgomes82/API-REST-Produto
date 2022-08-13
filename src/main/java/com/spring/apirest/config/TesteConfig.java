59f73435dbbde79c06c164f28e9018a4cda429c7
ulissesgomes82 <ulissesdeveloper82@gmail.com>
2022-08-12 22:38:04 -0300

    Implementando a classes TestConfi para instanciar produtos no BD

---
+++ src/main/java/com/spring/apirest/config/TesteConfig.java
@@ -0,0 +1,28 @@
+package com.spring.apirest.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.spring.apirest.model.Produto;
+import com.spring.apirest.repositories.ProdutoRepository;
+
+@Configuration
+public class TesteConfig implements CommandLineRunner {
+	@Autowired
+	ProdutoRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Produto p1 = new Produto(null, "Panela", 15.50, 25);
+		Produto p2 = new Produto(null, "Balde", 5.50, 15);
+		Produto p3 = new Produto(null, "Frigideira", 30.00, 21);
+		Produto p4 = new Produto(null, "Copo", 3.50, 12);
+		
+		repo.saveAll(Arrays.asList(p1,p2,p3,p4));
+	}
+
+}
