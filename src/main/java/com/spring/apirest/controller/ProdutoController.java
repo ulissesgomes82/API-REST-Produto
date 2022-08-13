c0c04c4fa204a12fa6dc6704ad7e53511509ca81
ulissesgomes82 <ulissesdeveloper82@gmail.com>
2022-08-12 22:31:27 -0300

    Implementando as classes ProdutoController e ProdutoService

---
+++ src/main/java/com/spring/apirest/controller/ProdutoController.java
@@ -0,0 +1,54 @@
+package com.spring.apirest.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.spring.apirest.model.Produto;
+import com.spring.apirest.service.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProdutoController {
+
+	@Autowired
+	ProdutoService service;
+
+	@GetMapping(value = "/produtos")
+	public ResponseEntity<List<Produto>> finAll() {
+		return ResponseEntity.ok(service.finAll());
+	}
+
+	@GetMapping(value = "/produto/{id}")
+	public ResponseEntity<Produto> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(service.findById(id));
+	}
+
+	@PostMapping(value = "/produtos")
+	public ResponseEntity<Produto> save(@RequestBody Produto produto) {
+		Produto obj = service.save(produto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/produto/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto produto){
+		return ResponseEntity.ok(service.update(id, produto));
+	}
+}
--- src/main/java/com/spring/apirest/service/ProdutoService.java
+++ src/main/java/com/spring/apirest/service/ProdutoService.java
@@ -1,8 +1,39 @@
 package com.spring.apirest.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.spring.apirest.model.Produto;
+import com.spring.apirest.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoService {
 
+	@Autowired
+	ProdutoRepository repository;
+
+	public List<Produto> finAll() {
+		return repository.findAll();
+	}
+
+	public Produto findById(Long id) {
+		Optional<Produto> obj = repository.findById(id);
+		return obj.get();
+	}
+
+	public Produto save(Produto produto) {
+		return repository.save(produto);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public Produto update(Long id, Produto produto) {
+		produto.setId(id);
+		return repository.save(produto);
+	}
 }
