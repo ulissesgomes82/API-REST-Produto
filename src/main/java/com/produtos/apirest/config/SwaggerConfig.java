8a974098122f97303b9a1229b1a42215dd95c747
ulissesgomes82 <ulissesdeveloper82@gmail.com>
2022-08-18 22:48:22 -0300

    Alterando uma configuração que estava dando erro

--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -9,7 +9,6 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,7 +28,7 @@ public class SwaggerConfig {
 
 		ApiInfo apiInfo = new ApiInfo("Produtos API REST", "API REST de cadastro de produtos.", "1.0",
 				"Terms of Service", new Contact("Ulisses Gomes", null, "ulissesgomes2005@gmail.com"),
-				"Apache License Version 2.0", "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
+				"Apache License Version 2.0", "https://www.apache.org/licesen.html", new ArrayList<>());
 
 		return apiInfo;
 	}
